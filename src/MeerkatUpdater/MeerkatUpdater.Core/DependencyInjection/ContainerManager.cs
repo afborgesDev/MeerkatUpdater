22df63048a39273b9d9ccff259f25f109a35b23b
Alexandre Borges <32342368+afborgesDev@users.noreply.github.com>
2020-04-13 13:28:40 -0300

    Preparing to dependency injection

---
+++ src/MeerkatUpdater/MeerkatUpdater.Core/DependencyInjection/ContainerManager.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MeerkatUpdater.Core.DependencyInjection
+{
+    public static class ContainerManager
+    {
+        public static IServiceCollection AddMeerkatUpdater(this IServiceCollection serviceContainer)
+        {
+            //Configurations
+            //Services?
+            //Executor
+            //serviceContainer.AddScoped
+
+            return serviceContainer;
+        }
+    }
+}
