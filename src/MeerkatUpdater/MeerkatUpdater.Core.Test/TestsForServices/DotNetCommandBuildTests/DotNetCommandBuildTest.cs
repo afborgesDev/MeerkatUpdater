805358bdb728fa78a1f1743ba49ffe604d541163
Alexandre Borges <32342368+afborgesDev@users.noreply.github.com>
2020-04-20 08:30:00 -0300

    starting tests for build'

---
+++ src/MeerkatUpdater/MeerkatUpdater.Core.Test/TestsForServices/DotNetCommandBuildTests/DotNetCommandBuildTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using MeerkatUpdater.Core.Test.GeneralUsage;
+using System.IO;
+using Xunit;
+
+namespace MeerkatUpdater.Core.Test.TestsForServices.DotNetCommandBuildTests
+{
+    public class DotNetCommandBuildTest
+    {
+        [Fact]
+        public void ShouldGenerateFilesASucceedBuild()
+        {
+            const string TestKey = "shoudGenerateFilesSucceedBuild";
+            try
+            {
+                var configManager = CreateCommandObjects.CreateConfigManager(TestKey);
+                var dotnetCommand = CreateCommandObjects.CreateDotNetCommand(configManager);
+                var build = CreateCommandObjects.CreateBuildCommand(configManager, dotnetCommand);
+
+                var result = build.Execute();
+                result.Should().BeTrue();
+
+                var files = Directory.EnumerateFiles(configManager.GetConfigurations().OutPutPath);
+                files.Should().HaveCountGreaterThan(0);
+            }
+            finally
+            {
+                CreatedFoldersManager.TierDownTest(TestKey);
+            }
+        }
+    }
+}
