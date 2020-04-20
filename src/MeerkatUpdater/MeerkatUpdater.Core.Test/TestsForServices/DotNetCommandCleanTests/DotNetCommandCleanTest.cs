a7c606f24bcbb54bd68891ab4b272cb61ba8c253
Alexandre Borges <32342368+afborgesDev@users.noreply.github.com>
2020-04-20 08:44:19 -0300

    added clean tests

---
+++ src/MeerkatUpdater/MeerkatUpdater.Core.Test/TestsForServices/DotNetCommandCleanTests/DotNetCommandCleanTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using MeerkatUpdater.Core.Test.GeneralUsage;
+using System.IO;
+using Xunit;
+
+namespace MeerkatUpdater.Core.Test.TestsForServices.DotNetCommandCleanTests
+{
+    public class DotNetCommandCleanTest
+    {
+        [Fact]
+        public void ShouldRemoveFilesFromOutPutFolderTheCleanExecution()
+        {
+            const string ProjectName = "MeerkatUpdater.Core.dll";
+            const string TestKey = "ShouldRemoveFilesFromOutPutFolderTheCleanExecution";
+            try
+            {
+                var configManager = CreateCommandObjects.CreateConfigManager(TestKey);
+                var configurations = configManager.GetConfigurations();
+                configurations.OutPutPath = Path.Combine(configurations.OutPutPath, "CleanOutPut");
+                Directory.CreateDirectory(configurations.OutPutPath);
+                configManager.SetConfigurations(configurations);
+                var dotNetcommand = CreateCommandObjects.CreateDotNetCommand(configManager);
+                var build = CreateCommandObjects.CreateBuildCommand(configManager, dotNetcommand);
+                var clean = CreateCommandObjects.CreateCleanCommand(configManager, dotNetcommand);
+
+                build.Execute();
+                clean.Execute();
+
+                var buildedProject = Path.Combine(configurations.OutPutPath, ProjectName);
+                var fileExists = File.Exists(buildedProject);
+                fileExists.Should().BeFalse();
+            }
+            finally
+            {
+                CreatedFoldersManager.TierDownTest(TestKey);
+            }
+        }
+    }
+}
